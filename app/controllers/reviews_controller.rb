ab3ef729b40a3d17e67f656554ea9314b4ea1fa9
omololaibojo <lola.polly@yahoo.it>
2019-05-13 14:57:34 +0100

    completed yelp exercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,30 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def index
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
